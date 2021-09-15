58b082d03bcfb63a5a8d07616c9829e60b02d010
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-09-15 15:17:45 +0530

    Threading&jQuery

---
+++ Day19/ThreadingExampleProject/ThreadingExampleProject/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingExampleProject
+{
+    class Program
+    {
+        void Sample1()
+        {
+            lock (this)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i + " from " + Thread.CurrentThread.Name);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        void Sample2()
+        {
+            for (int i = 10; i < 100; i=i+10)
+            {
+                Console.WriteLine(i + " from " + Thread.CurrentThread.Name);
+                Thread.Sleep(1000);
+            }
+        }
+        void UnderstandingTask()
+        {
+            //Task task = new Task(()=> {
+            //    for (int i = 10; i < 100; i = i + 10)
+            //    {
+            //        Console.WriteLine(i + " from " + Thread.CurrentThread.Name);
+            //        Thread.Sleep(1000);
+            //    }
+            //});
+            //task.Start();
+            Task<int> task = Task.Run(() => 10);
+
+            Console.WriteLine("The result from task is "+task.Result);
+        }
+        Task<string> GetTheTaskDone()
+        {
+            return Task.Run(()=> {
+                Thread.Sleep(30000);
+                return "Hello from Task";
+            });
+        }
+        async void PrintTaskValue()
+        {
+            string value =  await GetTheTaskDone();
+            Console.WriteLine(value); 
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.PrintTaskValue();
+            //Thread t1 = new Thread(program.Sample1);
+            //t1.Name = "Thread1";
+            //Thread t2 = new Thread(program.Sample1);
+            //t2.Name = "Thread2";
+            //t1.Start();
+            //t2.Start();
+            //program.Sample1();
+            //program.Sample2();
+            Console.ReadKey();
+        }
+    }
+}
