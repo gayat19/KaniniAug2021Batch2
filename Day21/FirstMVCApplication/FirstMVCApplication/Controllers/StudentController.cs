14296926464e6d89d69c0d478e403306f7e30f99
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-09-20 14:36:09 +0530

    MVC Intro

---
+++ Day21/FirstMVCApplication/FirstMVCApplication/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using FirstMVCApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        //It has to be available for all the action
+        static List<Student> students = new List<Student>();
+        //Action method
+        public IActionResult Index()
+        {
+            //int[] score = { 100, 89, 97 };
+            //ViewBag.Name = "Tim"; // new - dynamic- enumerable no casting is needed
+            //ViewData["Username"] = "userTim";//old - dictionary- fast- enumerable type casting is required
+            //ViewBag.Scores = score;
+            //ViewData["Scores"] = score;
+            //Student student = new Student();
+            //student.Id = 101;
+            //student.Name = "Tim";
+            //student.Age = 21;
+            //ViewBag.Student = student;
+            return View(students);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            students.Add(student);
+            return RedirectToAction("Index");
+        }
+    }
+}
